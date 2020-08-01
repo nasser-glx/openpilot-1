b71676197d60d80b87dbf1c1504a1fbd5a298d8f
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2020-08-01 10:12:14 -0700

    fix ui.cc

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -764,9 +764,6 @@ int main(int argc, char* argv[]) {
 
   const int MIN_VOLUME = LEON ? 12 : 9;
   const int MAX_VOLUME = LEON ? 15 : 11;
-  if(s->scene.controls_state.getVEgo() > 29) {
-    MAX_VOLUME = 0;
-  }
   assert(s->sound.init(MIN_VOLUME));
 
   int draws = 0;
